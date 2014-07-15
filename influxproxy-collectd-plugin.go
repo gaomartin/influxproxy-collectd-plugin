3e445f9bb22e7f61a74dc47d06c61605e8d73699
Daniel Menet <membership@sontags.ch>
2014-07-15 20:45:38 +0200

    adopted latest plugin infrastructur

--- influxproxy-collectd-plugin.go
+++ influxproxy-collectd-plugin.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"time"
+
 	"github.com/influxproxy/influxproxy/plugin"
 )
 
+type Functions struct{}
+
+func (f Functions) Ping() bool {
+	return true
+}
+
+func (f Functions) Describe() string {
+	return "Describe"
+}
+
+func (f Functions) Run(in []*interface{}) string {
+	return "Run"
+}
+
 func main() {
+	f := Functions{}
 	p, _ := plugin.NewPlugin()
-	p.Run()
+	p.Run(f)
 	time.Sleep(30 * time.Second)
 }
