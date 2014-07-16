b69aa8a617daa8091be173758d90c1b96ee0be87
Daniel Menet <membership@sontags.ch>
2014-07-16 19:01:47 +0200

    made use of the Description struct

--- influxproxy-collectd-plugin.go
+++ influxproxy-collectd-plugin.go
@@ -10,8 +10,24 @@ func (f Functions) Ping() bool {
 	return true
 }
 
-func (f Functions) Describe() string {
-	return "Describe"
+func (f Functions) Describe() plugin.Description {
+	args := new([]plugin.Argument)
+
+	arg := plugin.Argument{
+		Name:        "test",
+		Description: "test",
+		Optional:    true,
+	}
+
+	*args = append(*args, arg)
+
+	d := plugin.Description{
+		Description: "This plugin works as endpoint for CollectD, feeding the posted data into the given InfluxDB",
+		Author:      "githob.com/sontags",
+		Version:     "0.1.0",
+		Arguments:   *args,
+	}
+	return d
 }
 
 func (f Functions) Run(in []*interface{}) string {
@@ -22,4 +38,17 @@ func main() {
 	f := Functions{}
 	p, _ := plugin.NewPlugin()
 	p.Run(f)
+}
+
+type Description struct {
+	Description string
+	Author      string
+	Version     string
+	Arguments   []*Argument
+}
+
+type Argument struct {
+	Name        string
+	Description string
+	Optional    bool
 }
